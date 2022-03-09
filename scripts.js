86c32eb5fc284d407bdb9f2f6b97146aed018c64
jack mcqueen <jdmcqueen44@gmail.com>
2022-03-09 16:39:26 +0000

    Build basic function of display showing value of button clicked

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 // group id attributes of buttons into array
+
+const expression = () => {
+    const display = document.querySelector(".calculation p");
+    const btns = document.querySelectorAll("button");
+
+    btns.forEach(btn => btn.addEventListener("click", (e) => {
+        let character = e.target.value;
+
+        if (character === "AC") {
+
+        } else if (character === "CE") {
+            display.textContent = 0;
+        } else if (character === "%") {
+            
+        }
+
+        display.textContent = character;
+    }));
+}
+
+expression();
