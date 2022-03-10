f61870c62c59a3e64cf23fa0e94b3c0e62c8efc7
jack mcqueen <jdmcqueen44@gmail.com>
2022-03-10 14:03:55 +0000

    Change button conditional to switch/case; removed a comment

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// group id attributes of buttons into array
-
 const expression = () => {
     const display = document.querySelector(".calculation p");
     const btns = document.querySelectorAll("button");
@@ -7,15 +5,42 @@ const expression = () => {
     btns.forEach(btn => btn.addEventListener("click", (e) => {
         let character = e.target.value;
 
-        if (character === "AC") {
-
-        } else if (character === "CE") {
-            display.textContent = 0;
-        } else if (character === "%") {
+        switch (character) {
+            case "AC":
+                break;
             
-        }
+            case "CE":
+                display.textContent = 0;
+                break;
 
-        display.textContent = character;
+            case "&#37;":
+                // hundredth
+                break;
+            
+            case "&#247;":
+                // divide
+                display.textContent = character;
+                break;
+            
+            case "&#215;":
+                // multiply
+                break;
+
+            case "&#8722;":
+                // subtract
+                break;
+
+            case "&plus;":
+                // add
+                break;
+
+            case "&equals;":
+                // calculate
+                break;
+
+            default:
+                display.textContent = character;
+        }
     }));
 }
 
